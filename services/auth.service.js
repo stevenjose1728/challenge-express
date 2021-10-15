bcafb8dcf34b74f8c552f5bd9e96540b958efc5a
Steven <stevenvarela98@gmail.com>
2021-10-15 11:06:13 -0400

    :wrench: servicio to login

---
+++ services/auth.service.js
@@ -0,0 +1,42 @@
+const boom = require('@hapi/boom');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+
+const { config } = require('./../config/config');
+const UserService = require('./user.service');
+const service = new UserService();
+
+class AuthService {
+
+  async getUser(email, password) {
+    const user = await service.findByEmail(email);
+    if (!user) {
+      throw boom.unauthorized();
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw boom.unauthorized();;
+    }
+    delete user.dataValues.password;
+    return user;
+  }
+
+  signToken(_user) {
+    let user = {... _user}
+    if(_user.password){
+      delete user.password
+    }
+    const payload = {
+      sub: user.id,
+      role: user.role
+    }
+    const token = jwt.sign(payload, config.jwtSecret);
+    return {
+      user,
+      token
+    };
+  }
+}
+
+module.exports = AuthService;
