45b70eb49096cb668f755c3b515b3791c36ac8e5
Steven <stevenvarela98@gmail.com>
2021-10-19 11:02:08 -0400

    :wrench: validate optional pw

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -3,20 +3,23 @@ const Joi = require('joi');
 const id = Joi.number().integer();
 const email = Joi.string().email();
 const password = Joi.string().min(8);
-const role = Joi.string().min(5);
 
 const createUserSchema = Joi.object({
   email: email.required(),
   password: password.required(),
   role: Joi.string().required(),
-  password: Joi.string().required(),
   name: Joi.string().required(),
+  password: Joi.string().required(),
   password_confirmation: Joi.ref('password'),
 });
 
 const updateUserSchema = Joi.object({
-  email: email,
-  role: role,
+  email: email.required(),
+  password: password.required(),
+  role: Joi.string().required(),
+  name: Joi.string().required(),
+  password: Joi.string().optional(),
+  password_confirmation: Joi.ref('password'),
 });
 
 const getUserSchema = Joi.object({
