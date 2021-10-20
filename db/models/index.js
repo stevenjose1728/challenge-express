4608f8750c5b94b6016d3c1fd37bc90af9b32bc1
Steven <stevenvarela98@gmail.com>
2021-10-20 12:07:48 -0400

    :wrench: importing new models of accounts

--- db/models/index.js
+++ db/models/index.js
@@ -1,8 +1,11 @@
 const { User, UserSchema } = require('./user.model');
+const {Account, AccountSchema} = require('./account.model')
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
+  Account.init(AccountSchema, Account.config(sequelize));
   User.associate(sequelize.models);
+  Account.associate(sequelize.models);
 }
 
 module.exports = setupModels;
