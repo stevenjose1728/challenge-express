799d2477e3c4e9cfa5c6f4ff2ed17e4aa55349fb
Steven <stevenvarela98@gmail.com>
2021-10-21 09:34:40 -0400

    :bug: changing schema keys in create

--- schemas/account.schema.js
+++ schemas/account.schema.js
@@ -1,12 +1,13 @@
 const Joi = require('joi');
 
 const createAccountSchema = Joi.object({
-  created_at: Joi.optional(),
-  deleted_at: Joi.optional().allow(null),
+  createdAt: Joi.optional(),
+  deletedAt: Joi.optional().allow(null),
   name: Joi.string().required(),
   responsableId: Joi.number().required(),
   teamConsultation: Joi.string().required(),
-  updated_at: Joi.optional(),
+  updatedAt: Joi.optional(),
+  id: Joi.number().optional(),
   userId: Joi.number().required()
 });
 
