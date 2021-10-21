9c1a2e462ddfe55e90292a9505f10ff6bffa6134
Steven <stevenvarela98@gmail.com>
2021-10-21 09:52:16 -0400

    :wrench: schema to update accounts

--- schemas/account.schema.js
+++ schemas/account.schema.js
@@ -7,10 +7,21 @@ const createAccountSchema = Joi.object({
   responsableId: Joi.number().required(),
   teamConsultation: Joi.string().required(),
   updatedAt: Joi.optional(),
-  id: Joi.number().optional(),
+  userId: Joi.number().required()
+});
+
+const updateUserSchema = Joi.object({
+  createdAt: Joi.optional(),
+  deletedAt: Joi.optional().allow(null),
+  name: Joi.string().required(),
+  responsableId: Joi.number().required(),
+  teamConsultation: Joi.string().required(),
+  updatedAt: Joi.optional(),
+  id: Joi.number().required(),
   userId: Joi.number().required()
 });
 
 module.exports = {
-  createAccountSchema
+  createAccountSchema,
+  updateUserSchema
 }
